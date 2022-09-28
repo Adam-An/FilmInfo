9304c4d3a4a7af68a86a1c51c9c84ce9bef31721
Adam Anthony <68133111+Adam-An@users.noreply.github.com>
2022-09-28 18:33:06 +0100

    first commit

---
+++ src/main/java/com/example/filminfo/FilmInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.filminfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmInfoApplication.class, args);
+    }
+
+}
